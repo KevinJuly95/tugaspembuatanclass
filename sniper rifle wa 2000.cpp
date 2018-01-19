f4224ed06ccbf03539615e5ba44626a2d8a1fbbc
KevinJuly95 <32257783+KevinJuly95@users.noreply.github.com>
2018-01-19 10:38:34 +0700

    class 1

---
+++ sniper rifle wa 2000.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class sniperrifle{
+	public:
+		void senapan(string tembak);
+			 Menembak(string Menembak);
+			 Senjata(string Senjata);
+			 Air_soft_gun(string Air_soft_gun);
+			 Langka(string Langka); 
+		string rifle, tipe, negara, oleh, tahun, produsen, tembakan;
+		int jumlah, panjang1, panjang2, jarak; 
+		double berat1, berat2;
+};

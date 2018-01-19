7e557cff574217547be97378bed562897ee07c21
KevinJuly95 <32257783+KevinJuly95@users.noreply.github.com>
2018-01-19 10:39:48 +0700

    class 2

--- sniper rifle wa 2000.cpp
+++ sniper rifle wa 2000.cpp
@@ -13,3 +13,25 @@ class sniperrifle{
 		int jumlah, panjang1, panjang2, jarak; 
 		double berat1, berat2;
 };
+
+int main() {
+	sniperrifle walther;
+cout<<"\n\t\t\tNAMA      : Kevin Juli Reksi\n";
+cout<<"\t\t\tNPM       : 16630474\n";
+cout<<"\t\t\tPROGRAM   : Teknik Informtika\n";
+cout<<"\t\t\tTentang   : Senapan Runduk Walther WA-2000\n";
+
+	cout<<"\n";
+ 	walther.tipe     = "Senapan runduk \n";
+ 	walther.negara   = "Jerman \n";
+ 	walther.oleh     = "Polisi Jerman \n";
+ 	walther.tahun    = "1970s-1980an  \n";
+ 	walther.produsen = "1982-1988 \n";
+ 	walther.jumlah   =  176;
+ 	walther.berat1   =  6.95;
+ 	walther.berat2   =  7.35;
+ 	walther.panjang1 =  905;
+ 	walther.panjang2 =  650;
+ 	walther.tembakan =  "Semi otomatis \n";
+ 	walther.jarak     =  1000;
+ 	
